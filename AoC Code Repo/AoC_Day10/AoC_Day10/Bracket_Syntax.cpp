e7bea818120f76f7653e6d1a3b26ff505faf5ad2
Alistair Brown <ali.b_houghton@hotmail.com>
2021-12-10 11:01:04 +0000

    Day 10

---
+++ AoC Code Repo/AoC_Day10/AoC_Day10/Bracket_Syntax.cpp
@@ -0,0 +1,105 @@
+#include "Bracket_Syntax.h"
+
+int Syntax::Bracket::DoesCharMatchAsBracket(char potentialBracket)
+{
+	if (potentialBracket == openChar) { return OpenBracket; }
+	else if (potentialBracket == closeChar) { return CloseBracket; }
+	else { return NotMatchingBracket; }
+}
+
+void Syntax::SyntaxChecker::InsertCompletionCostIntoOrder(unsigned long long int cost)
+{
+	if (completionCosts.size() == 0)
+	{
+		completionCosts.push_back(cost);
+	}
+	else
+	{
+		bool inserted{ false };
+		for (std::list<unsigned long long int>::iterator iter = completionCosts.begin();
+			 iter != completionCosts.end();
+			 iter++)
+		{
+			if (cost < *iter)
+			{
+				completionCosts.insert(iter, cost);
+				inserted = true;
+				break;
+			}
+		}
+		if (!inserted) { completionCosts.push_back(cost); }
+	}
+}
+
+void Syntax::SyntaxChecker::TryToInsertLine(std::string line)
+{
+	std::vector<char> openBracketStack{};
+	int corruptedBracketValue{ 0 };
+	unsigned long long int completionCost{ 0 };
+	
+	for (char nextBracket : line)
+	{
+		for (Bracket bracketType : bracketTypes)
+		{
+			int bracketMatch = bracketType.DoesCharMatchAsBracket(nextBracket);
+			if (bracketMatch == Bracket::OpenBracket)
+			{
+				openBracketStack.push_back(nextBracket);
+				break;
+			}
+			else if (bracketMatch == Bracket::CloseBracket)
+			{
+				if (bracketType.DoesCharMatchAsBracket(openBracketStack.back())
+					== Bracket::OpenBracket)
+				{
+					openBracketStack.pop_back();
+				}
+				else
+				{
+					corruptedBracketValue = bracketType.CorruptedBracketValue();
+				}
+				break;
+			}
+		}
+		if (corruptedBracketValue > 0)
+		{
+			totalCorruption += corruptedBracketValue;
+			break;
+		}
+	}
+
+	if (corruptedBracketValue == 0)
+	{
+		while (openBracketStack.size() > 0)
+		{
+			char nextBracket = openBracketStack.back();
+			openBracketStack.pop_back();
+			for (Bracket bracketType : bracketTypes)
+			{
+				int bracketMatch = bracketType.DoesCharMatchAsBracket(nextBracket);
+				if (bracketMatch == Bracket::OpenBracket)
+				{
+					unsigned long long int lastCompletionCost = completionCost;
+					completionCost = (completionCost * 5) + bracketType.CompletionBracketValue();
+					if (lastCompletionCost > completionCost)
+					{
+						int ii = 5;
+					}
+				}
+			}
+		}
+		InsertCompletionCostIntoOrder(completionCost);
+	}
+}
+
+unsigned long long int Syntax::SyntaxChecker::MiddleCompletionCost()
+{
+	// Let's just assume an odd number of element
+	int middlePosition = (completionCosts.size() / 2) + 1;
+	std::list<unsigned long long int>::iterator iter = completionCosts.begin();
+	for (int ii = 1; ii < middlePosition; ii++)
+	{
+		iter++;
+	}
+	return *iter;
+}
---
+++ AoC Code Repo/AoC_Day10/AoC_Day10/Bracket_Syntax.h
@@ -0,0 +1,42 @@
+#pragma once
+#include <vector>
+#include <string>
+#include <list>
+
+namespace Syntax
+{
+	class Bracket
+	{
+	private:
+		char openChar;
+		char closeChar;
+		int corruptedBracketValue;
+		int completionBracketValue;
+	public:
+		static const int NotMatchingBracket{ 0 };
+		static const int OpenBracket{ 1 };
+		static const int CloseBracket{ 2 };
+		Bracket(char openChar, char closeChar, int corruptedBracketValue, int completionBracketValue) :
+			openChar{ openChar },
+			closeChar{ closeChar },
+			corruptedBracketValue{ corruptedBracketValue },
+			completionBracketValue{ completionBracketValue }{};
+		int DoesCharMatchAsBracket(char potentialBracket);
+		int CorruptedBracketValue() { return corruptedBracketValue; }
+		int CompletionBracketValue() { return completionBracketValue; }
+	};
+
+	class SyntaxChecker
+	{
+	private:
+		std::vector<Bracket> bracketTypes;
+		int totalCorruption{ 0 };
+		std::list<unsigned long long int> completionCosts{};
+		void InsertCompletionCostIntoOrder(unsigned long long int cost);
+	public:
+		SyntaxChecker(std::vector<Bracket> bracketTypes) : bracketTypes{ bracketTypes } {};
+		void TryToInsertLine(std::string line);
+		int TotalCorruption() { return totalCorruption; }
+		unsigned long long int MiddleCompletionCost();
+	};
+}
