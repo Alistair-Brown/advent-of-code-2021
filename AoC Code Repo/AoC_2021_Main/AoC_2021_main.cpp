4a952a01f68dece6019758dba94b4caa138facaf
Alistair Brown <ali.b_houghton@hotmail.com>
2022-04-16 17:21:31 +0100

    Edits to main to allow the user to either specify a single puzzle to solve or solve every day (only day 1 is currently hooked up to main).

--- AoC Code Repo/AoC_2021_Main/AoC_2021_main.cpp
+++ AoC Code Repo/AoC_2021_Main/AoC_2021_main.cpp
@@ -3,12 +3,56 @@
 #include "puzzle_solvers.h"
 #include <iostream>
 
+// Based on user input either solve and print the result for a single day, or run through
+// solving and printing the results for all puzzles.
 int main()
 {
-	// To test the architecture, temporarily just call the puzzle solver for day
-	// 1 and print the output for manual comparison with the known answer.
-	std::ifstream myIfStream = InputRetrieval::GetInputStreamForPuzzleInput(1);
-	PuzzleAnswerPair dayOneAnswers = PuzzleSolvers::puzzleSolvers[0](myIfStream);
-	std::cout << dayOneAnswers.first << ", " << dayOneAnswers.second << std::endl;
+	int dayToRun{ 0 };
+	bool runAll{ false };
+	while ((dayToRun <= 0) || (dayToRun > PuzzleSolvers::numberOfPuzzleSolvers))
+	{
+		std::cout << "Enter a number between 1 and " << PuzzleSolvers::numberOfPuzzleSolvers <<
+			" to solve that day's puzzle, or \"all\" to solve all days: ";
+		std::string dayAsString;
+
+		std::cin >> dayAsString;
+		if (dayAsString == "all")
+		{
+			runAll = true;
+			break;
+		}
+		else
+		{
+			dayToRun = atoi(dayAsString.c_str());
+		}
+	}
+	std::cout << std::endl;
+
+	if (runAll)
+	{
+		// We are solving every puzzle. We have an array of puzzle solvers provided to us
+		// by the PuzzleSolver namespace, so this is a simple case of iterating over
+		// that array and printing readable results as we go.
+		unsigned int day = 1;
+		std::ifstream puzzleInput;
+		for (PuzzleFunc puzzleSolver : PuzzleSolvers::puzzleSolvers)
+		{
+			puzzleInput = InputRetrieval::GetInputStreamForPuzzleInput(day);
+			PuzzleAnswerPair answerPair = puzzleSolver(puzzleInput);
+			std::cout << "Day " << day << ": " << answerPair.first << ", " << answerPair.second << std::endl;
+			day++;
+		}
+	}
+	else
+	{
+		// Just solve a single day's puzzle and print the result, with a comma separating
+		// the result for part 1 and 2.
+		// And of course, be careful of the fact that day numbering starts from 1, but
+		// our array of puzzle solvers is 0-indexed.
+		std::ifstream puzzleInput = InputRetrieval::GetInputStreamForPuzzleInput(dayToRun);
+		PuzzleAnswerPair answerPair = PuzzleSolvers::puzzleSolvers[dayToRun - 1](puzzleInput);
+		std::cout << "Day " << dayToRun << ": " << answerPair.first << ", " << answerPair.second << std::endl;
+	}
+
 	return 0;
 }
